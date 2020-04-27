d6c361808a41be717c8719d5c8b0c37b0fca3291
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-04-27 12:58:16 +0300

    Add missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { PhoneComponent } from './phone/phone.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, LandingPageModule],
+  declarations: [AppComponent, PhoneComponent],
+  imports: [BrowserModule, LandingPageModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
