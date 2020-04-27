c27c8f1e00a5fbf9c7977fb3272029079274648d
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2020-04-27 14:32:14 +0300

    Add info section

---
+++ src/app/landing-page/info-section/info-section.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { InfoSectionComponent } from './info-section.component';
+
+@NgModule({
+  declarations: [InfoSectionComponent],
+  imports: [],
+  exports: [InfoSectionComponent],
+})
+export class InfoSectionModule {}
