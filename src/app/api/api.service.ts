189755471d5bd37722ac5434d380854a184001b2
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-04-27 16:01:05 +0300

    Cleanup

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
@@ -42,6 +42,6 @@ export class ApiService {
         `body was: ${error.error}`);
     }
     // return an observable with a user-facing error message
-    return throwError('Something bad happened; please try again later.');
+    return throwError('Something bad happened; please try again later.')
   };
 }
