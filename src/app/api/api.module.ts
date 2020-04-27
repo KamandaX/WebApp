30a97ba407f4324838c7c18848a8323e7cc8898d
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-04-27 16:06:48 +0300

    Cleanup

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { QuizComponent } from '../quiz/quiz.component';
 
 
-
 @NgModule({
   declarations: [QuizComponent],
-  imports: [],
   exports: [QuizComponent]
 })
 export class ApiModule { }
