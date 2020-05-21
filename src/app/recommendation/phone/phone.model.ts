a804f1a4104b9e26174b78aa0a7271922bdf90fb
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2020-05-21 20:09:53 +0300

    Add recommendation, phone and camera models

---
+++ src/app/recommendation/phone/phone.model.ts
@@ -0,0 +1,41 @@
+import { Expose, Type } from 'class-transformer';
+import Camera from './camera.model';
+
+export default class Phone {
+  manufacturer: string;
+
+  @Expose({ name: 'model_name' })
+  model: string;
+
+  @Expose({ name: 'display_resoliution_horizontal' })
+  displayResoliutionHorizontal: string;
+
+  @Expose({ name: 'display_resoliution_vertical' })
+  displayResoliutionVertical: string;
+
+  price: number;
+
+  @Expose({ name: 'launch_date' })
+  launchDate: string;
+
+  @Expose({ name: 'screen_size' })
+  screenSize: number;
+
+  @Expose({ name: 'protection_rating' })
+  protectionRating: number;
+
+  processor: string;
+
+  storage: string;
+
+  ram: number;
+
+  @Expose({ name: 'battery_size' })
+  batterySize: number;
+
+  @Type(() => Camera)
+  cameras: Camera[];
+
+  @Expose({ name: 'image_url' })
+  imageUrl: string;
+}
